07fe39586edc7a142a18c089f6efdab9fc06ce6e
Raz <raz@trezlabs.com>
2021-11-25 21:57:27 -0600

    remove comment from app controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,8 +13,7 @@ class ApplicationController < ActionController::API
       end
   
       def current_user
-        # current_user ||= session[:user_id] && User.find_by_id(session[:user_id])
-        User.first
+        current_user ||= session[:user_id] && User.find_by_id(session[:user_id])
       end
   
   
