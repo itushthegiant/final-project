284472d2bbf7365830112c5b3966b924cf205f20
etamar <etamar.levy@gmail.com>
2022-01-27 19:48:29 -0500

    cors test

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'
+    origins 'http://3.132.216.5:4000'
     resource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head],
