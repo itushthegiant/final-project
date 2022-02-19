3bd620e21124bd4fa179e7432052675bf1d42741
etamar <etamar.levy@gmail.com>
2022-02-19 16:03:06 -0500

    foo

--- app/serializers/concerns/image_concern.rb
+++ app/serializers/concerns/image_concern.rb
@@ -3,6 +3,6 @@
       include Rails.application.routes.url_helpers
   
       included do
-        Rails.application.routes.default_url_options[:host] = 'http://localhost:3000'
+        Rails.application.routes.default_url_options[:host] = 'http://linkedserv.com:3000'
       end
     end
