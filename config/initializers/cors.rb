bf8cad8199cd1d045b5d2c4a33ec3c5c7f30775d
etamar <etamar.levy@gmail.com>
2022-02-19 14:49:42 -0500

    foo

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://linkedserv.com:4000'
+    origins 'http://www.linkedserv.com:4000'
     resource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head],
